d9911ca83078ada4af17115157db305362f65080
chlovers <chloie_day@hotmail.com>
2024-10-21 15:28:25 -0300

    changed camera, deleted a building, added a ladder

--- Assets/scripts/playermovement.cs
+++ Assets/scripts/playermovement.cs
@@ -6,7 +6,6 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-
     private Rigidbody rb;
     private float movementX;
     private float movementY;
@@ -14,24 +13,22 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 2.0f;
     public bool isGrounded;
     public float speed = 0;
-    
+    public float climbSpeed = 3.0f; // Speed for climbing
+    private bool isClimbing = false; // Flag for climbing
 
     void Start()
     {
-
         rb = GetComponent<Rigidbody>();
         jump = new Vector3(0.0f, 5.0f, 0.0f);
     }
 
-
     void OnMove(InputValue movementValue)
     {
-
         Vector2 movementVector = movementValue.Get<Vector2>();
-
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+
     void OnCollisionStay()
     {
         isGrounded = true;
@@ -39,21 +36,45 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
-
-        rb.AddForce(movement * speed);
+        if (isClimbing)
+        {
+            // Climbing movement
+            Vector3 climbMovement = new Vector3(0.0f, movementY, 0.0f); // Only vertical movement for climbing
+            rb.velocity = new Vector3(rb.velocity.x, climbMovement.y * climbSpeed, rb.velocity.z);
+        }
+        else
+        {
+            // Regular movement
+            Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+            rb.AddForce(movement * speed);
+        }
     }
 
     void Update()
     {
-        // Handle jumping
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        // Handle jumping
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !isClimbing)
         {
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Climbable")) // Ensure your ladder has the "Ladder" tag
+        {
+            isClimbing = true;
+            rb.useGravity = false; // Disable gravity while climbing
+        }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Climbable"))
+        {
+            isClimbing = false;
+            rb.useGravity = true; // Enable gravity again when exiting the ladder
+        }
+    }
+}
